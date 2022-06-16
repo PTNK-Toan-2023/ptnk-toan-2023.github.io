a29c410a7001c7a72ea6789251ea283d78acb75c
poigiatre <85273780+poigiatre@users.noreply.github.com>
2022-06-16 22:15:10 +0700

    Add files via upload

---
+++ js/members.js
@@ -0,0 +1,285 @@
+var mem = [
+    {
+        "name": "Nguy\u1ec5n Ph\u1ea1m Kh\u00e1nh An",
+        "hobby": "\u0102n, ng\u1ee7, Olympia",
+        "review": "- Kh\u00f4ng c\u00f3 tu\u1ed5i kh\u00e1ng anh\n- C\u1ef1u l\u1edbp tr\u01b0\u1edfng",
+        "quote": "Mu\u1ed1n nhanh th\u00ec ph\u1ea3i t\u1eeb t\u1eeb",
+        "other": null
+    },
+    {
+        "name": "Nguy\u1ec5n Nam Anh",
+        "hobby": "V\u0103n v\u00e0 nh\u00e2n v\u1eadt alime",
+        "review": "- T\u00f3c xo\u0103n nh\u01b0ng t\u00e2m h\u1ed3n \u1edf 235 Nguy\u1ec5n V\u0103n C\u1eeb",
+        "quote": "L\u00ea H\u1ed3ng Phong l\u00e0 lu\u00f4n h\u1ea1nh ph\u00fac",
+        "other": null
+    },
+    {
+        "name": "Nguy\u1ec5n Nh\u1eadt Anh",
+        "hobby": "Genshin+Danmaku",
+        "review": "- Th\u00e0nh vi\u00ean h\u1ed9i board game\n- Wjbu",
+        "quote": "\"zui\"",
+        "other": null
+    },
+    {
+        "name": "Tr\u1ea7n Th\u1ecb Qu\u1ef3nh Anh",
+        "hobby": "B\u1ea5t c\u1ee9 \u0111i\u1ec1u g\u00ec l\u00e0m Uy\u00ean kh\u00f4ng vui",
+        "review": "- Ch\u1ecb iu tuy\u1ec7t v\u1eddi \u00f4ng m\u1eb7t tr\u1eddi \u2665\ufe0f \ud83d\udd06",
+        "quote": "Kh\u00f4ng bi\u1ebft ph\u1ea3i ghi g\u00ec :)",
+        "other": null
+    },
+    {
+        "name": "Nguy\u1ec5n T\u1ea5t B\u00e1ch",
+        "hobby": "L\u00e0m tr\u00f9m Dream league soccer",
+        "review": "- Ng\u01b0\u1eddi ch\u0103m h\u1ecdc nh\u1ea5t l\u1edbp, 8h t\u1ed1i v\u1eabn c\u00f2n trong Meeting",
+        "quote": "Nguy\u1ec5n Ph\u1ea1m Thu\u1eadn Thi\u00ean l\u00e0 h\u1ecdc tr\u00f2 c\u1ee7a t\u00f4i",
+        "other": null
+    },
+    {
+        "name": "Tr\u1ea7n Lam Bi\u1ec3u",
+        "hobby": "Ng\u1ee7, v\u1ebd tranh, xem anime, fan c\u1ee7a FromSoftware ",
+        "review": "- \u0110en\n- V\u1ebd \u0111\u1eb9p\n- V\u1ebd \u1ea3nh valentine si\u00eau \u0111\u1eb9p\n- Edit video gi\u1ecfi\n- Gi\u1ecdng \u1ea5m",
+        "quote": "Ng\u1ee7 l\u00e0 s\u1ee9c m\u1ea1nh",
+        "other": null
+    },
+    {
+        "name": "\u0110\u1eb7ng Gia Ch\u00ed",
+        "hobby": "Tr\u1edf th\u00e0nh hokage, tr\u1edf th\u00e0nh m\u1ed9t vi\u00eam tr\u1ee5 ho\u1eb7c tr\u1edf th\u00e0nh ch\u00fa thu\u1eadt s\u01b0 c\u1ea5p m\u1ed9t",
+        "review": "- K\u1ebb h\u1ee7y di\u1ec7t Powerpoint",
+        "quote": "Kh\u00f4ng s\u1ee3 k\u1ebb \u0111\u1ecbch m\u1ea1nh, ch\u1ec9 s\u1ee3 k\u1ebb \u0111\u1ecbch l\u00e0 khoa l\u00e9 n\u00ean kh\u00f4ng d\u00e1m \u0111\u00e1nh >=(",
+        "other": null
+    },
+    {
+        "name": "Tr\u01b0\u01a1ng Qu\u1ed1c C\u01b0\u1eddng",
+        "hobby": "- sleep\n- \u0111am m\u00ea h\u1ecdc wrirting ielts",
+        "review": "- T\u1ee5t \u0111\u01b0\u1eddng huy\u1ebft\n- Nghi\u1ec7n\n- rich kid iphone 12",
+        "quote": "Kh\u00f4ng bi\u1ebft \u0111\u1ea7n \u0111\u00e2u m\u00e0 l\u01b0\u1eddng th\u00ec sao m\u00e0 bi\u1ebft \u0111\u01b0\u1eddng \u0111\u00e2u m\u00e0 l\u1ea7n",
+        "other": null
+    },
+    {
+        "name": "L\u00ea Th\u00e0nh Danh",
+        "hobby": "Ng\u1ee7, t\u1ea5u h\u00e0i c\u00f9ng 11 To\u00e1n, th\u00edch m\u00e0u h\u1ed3ng v\u00e0 gh\u00e9t s\u1ef1 gi\u1ea3 d\u1ed1i",
+        "review": "- \u00d4ng tr\u00f9m m\u1ea5t m\u1ea1ng\n- K\u1ebb t\u00ean Danh nh\u01b0ng kh\u00f4ng wack duy nh\u1ea5t c\u1ee7a l\u1edbp",
+        "quote": "\u01af\u1edbc m\u01a1 gi\u00e0u \u0111\u1ebfn m\u1ef1c mua h\u1ebft \u0111\u01b0\u1ee3c t\u1ea5t c\u1ea3 c\u00e1c nh\u00e0 h\u00e0ng ti\u1ec7c c\u01b0\u1edbi",
+        "other": null
+    },
+    {
+        "name": "Tr\u1ea7n Nguy\u1ec5n Thanh Danh",
+        "hobby": "H\u00ecnh h\u1ecdc",
+        "review": "- Ch\u1ec9 th\u1eafng \u0111\u00e1nh b\u00e0i khi b\u00e0i m\u1ea1nh\n- Wack",
+        "quote": "Ch\u00e2m ng\u00f4n \u0111\u00e3 b\u1ecb tao ch\u00f4n, ng\u00e2m",
+        "other": "N\u00f3i v\u1edbi to\u00e1n h\u1ecdc: \"T\u00f4i y\u00eau em t\u1eeb c\u00e1i nh\u00ecn \u0111\u1ea7u ti\u00ean\""
+    },
+    {
+        "name": "Phan Tr\u1ecdng \u0110\u00e0i",
+        "hobby": "C\u1ecdc chanh v\u00e0 thi\u1ec1n",
+        "review": "- Dark qu\u00e0i\n- Vague\n- K\u1ebb \u0111\u1eb7t l\u1ea1i n\u1ec1n m\u00f3ng cho in l\u1ecbch v\u00e0 \u0111\u00e3 t\u1ea3i l\u1ecbch h\u01b0 m\u00e1y \n- Ng\u01b0\u1eddi active nh\u00ec tr\u00ean group l\u1edbp",
+        "quote": "H\u00f4ng kh\u1ecfi, khi b\u1ed9ng b\u00e0 y\u00eaus",
+        "other": null
+    },
+    {
+        "name": "Nguy\u1ec5n Th\u00e0nh \u0110\u1ea1t",
+        "hobby": "Ch\u01a1i game, \u0111\u00e1 banh",
+        "review": "- Tsundere >:((\n- Top 1 Flo",
+        "quote": "Quan tr\u1ecdng hay kh\u00f4ng kh\u00f4ng quan tr\u1ecdng quan tr\u1ecdng l\u00e0 c\u00f3 quan tr\u1ecdng hay kh\u00f4ng",
+        "other": null
+    },
+    {
+        "name": "Nguy\u1ec5n Ti\u1ebfn \u0110\u1ea1t",
+        "hobby": "Suy ngh\u0129",
+        "review": "- Beatboxer\n- \u00d4ng tr\u00f9m b\u1eafn kim c\u01b0\u01a1ng, h\u1ed9t xo\u00e0n\n- L\u1edbp ph\u00f3 c\u1edd vua\n- \u00d4ng tr\u00f9m \u0111\u01b0\u1eddng mid",
+        "quote": "Xin m\u1ed9t l\u1ea7n thua",
+        "other": null
+    },
+    {
+        "name": "Nguy\u1ec5n Tu\u1ea5n Hi\u1ec7p",
+        "hobby": "\u0110\u00e1 banh, Deku",
+        "review": "- Wjbu \u0111\u1eb9p trai",
+        "quote": "No.1",
+        "other": null
+    },
+    {
+        "name": "Mai Kh\u1eafc Hi\u1ebfu",
+        "hobby": "guitar, mkhdeptrai",
+        "review": "NAM THANG MOI DA DEN",
+        "quote": "bot nghien",
+        "other": null
+    },
+    {
+        "name": "L\u00e3 V\u0103n Ho\u00e0",
+        "hobby": "itHelp, \u0111\u00e1 banh ",
+        "review": "- L\u1eeda V\u0103n l\u00e0 H\u1ecfa V\u0103n m\u00e0 H\u1ecfa V\u0103n L\u00e0 L\u00e3 V\u0103n H\u00f2a\n- Ch\u1ea5m than cu\u1ed1i c\u00e2u",
+        "quote": "G\u00e0",
+        "other": null
+    },
+    {
+        "name": "Tr\u01b0\u01a1ng C\u00f4ng Huy Ho\u00e0ng",
+        "hobby": "\u0110\u00e1 banh",
+        "review": "- Sir, S\u1ec9\n- God but not crazy",
+        "quote": "B\u00ed",
+        "other": null
+    },
+    {
+        "name": "Ho\u00e0ng V\u0169 Gia Huy",
+        "hobby": "Game, phim+anime, workout, \u0103n u\u1ed1ng",
+        "review": "Yaoi, step Huy ",
+        "quote": "\"Dam\" ",
+        "other": null
+    },
+    {
+        "name": "L\u00ea Nguy\u1ec5n B\u1ea3o Huy",
+        "hobby": "Ng\u1ee7/game/c\u00e1i g\u00ec \u0111\u00f3 m\u00e0 c\u00f2n th\u00fa v\u1ecb",
+        "review": "- \u00d4ng tr\u00f9m n\u1ed1i t\u1eeb \n- Huy ch\u00e0o Huy",
+        "quote": "T\u01b0\u1edfng t\u01b0\u1ee3ng tui ghi g\u00ec th\u00fa v\u1ecb \u0111i",
+        "other": null
+    },
+    {
+        "name": "H\u1ed3 V\u0169 Minh Khang",
+        "hobby": "To\u00e1n, \u0111\u00e1 banh.",
+        "review": "K\u1ebb lu\u00f4n ch\u1ea5m c\u00e2u.",
+        "quote": ".",
+        "other": null
+    },
+    {
+        "name": "L\u00ea \u0110\u0103ng Khoa",
+        "hobby": "Ch\u01a1i game v\u00e0 m\u00e1t xa l\u01b0ng b\u1ea1n ",
+        "review": "- Thanos c\u00f3 th\u1ec3 \u0111\u1ea5m b\u1ea1n b\u1ec3 ph\u1ed5i\n- Th\u00e2n h\u00ecnh nh\u1ecf nh\u1eafn \u1ed1m \u00edu",
+        "quote": "Tr\u01b0\u1eddng PTNK khu qu\u1eadn 5, c\u00f2n t\u00f4i Khu Ngoa",
+        "other": null
+    },
+    {
+        "name": "Nguy\u1ec5n Tr\u01b0\u01a1ng \u0110\u0103ng Khoa",
+        "hobby": null,
+        "review": "- Rapper\n- Ch\u00faa t\u1ec3 gi\u1ea3 g\u00e1i ",
+        "quote": null,
+        "other": "Normie"
+    },
+    {
+        "name": "Mai Trung Ki\u00ean",
+        "hobby": "Game v\u00e0 piano",
+        "review": "- Gi\u00fap \u0111\u1ee1 b\u1ea1n b\u00e8\n- Mai bee nh\u01b0ng v\u1eabn t\u1ef1 th\u1ea5y ch\u01b0a \u0111\u1ee7 n\u00ean ng\u00e0y c\u00e0ng bee h\u01a1n\n- Ti\u1ec3u t\u01b0 s\u1ea3n\n- Ch\u01b0a c\u00f3 gf",
+        "quote": "M\u00ecnh kh\u00f4ng gi\u1ecfi b\u1eb1ng ng\u01b0\u1eddi kh\u00e1c th\u00ec m\u00ecnh b\u00f9 l\u1ea1i b\u1eb1ng Ki\u00ean tr\u00ec :D",
+        "other": null
+    },
+    {
+        "name": "Nguy\u1ec5n Anh Ki\u1ec7t",
+        "hobby": "C\u1ea7u l\u00f4ng, b\u00f3ng b\u00e0n, \u0103n ng\u1ee7... ",
+        "review": "Bay con B\u00ecnh \u0110\u1ecbnh",
+        "quote": "\u0111\u1edf teo, b\u1ee3n b\u00eay tr\u00e9nh re ",
+        "other": null
+    },
+    {
+        "name": "L\u00ea Nguy\u1ec5n B\u1ea3o Minh",
+        "hobby": "Th\u00edch CTF",
+        "review": "- Ng\u1ea7u\n- Ch\u1ec9 c\u1ea7n b\u1ea1n c\u00f3 m\u1eb7t, th\u1eafng thua kh\u00f4ng quan tr\u1ecdng",
+        "quote": "L\u00e0m g\u00ec c\u0169ng ph\u1ea3i ng\u1ea7u ;)",
+        "other": null
+    },
+    {
+        "name": "\u0110\u1ed7 Th\u00e0nh Nam",
+        "hobby": "B\u00f3ng, c\u00f3 t\u00e2m h\u1ed3n say m\u00ea c\u00e1i \u0111\u1eb9p :) ",
+        "review": "- Thanh ni\u00ean nghi\u00eam t\u00fac\n- Th\u00e1nh thuy\u1ebft tr\u00ecnh chay",
+        "quote": "\u0110\u00e3 l\u00e0m l\u00e0 l\u00e0m cho t\u1edbi, c\u00f2n kh\u00f4ng t\u1edbi th\u00ec \u0111i ng\u1ee7",
+        "other": "N\u1ed5 th\u00e0nh dam"
+    },
+    {
+        "name": "L\u00ea Anh Nh\u1eadt",
+        "hobby": "M\u1ed9t m\u00ecnh, ch\u01a1i game, \u0111\u1ecdc s\u00e1ch",
+        "review": "- WjBu\n- M\u1ed9t con ng\u01b0\u1eddi c\u00f3 \u0111am m\u00ea, h\u1ebft l\u00f2ng v\u00ec \u01b0\u1edbc m\u01a1 c\u1ee7a m\u00ecnh\n- C\u1eadu c\u00f3 t\u00e0i n\u0103ng l\u1eabn s\u1ef1 ch\u0103m ch\u1ec9.",
+        "quote": "M\u1ecdi th\u1ee9 \u0111\u1ec1u c\u00f3 nguy\u00ean nh\u00e2n c\u1ee7a n\u00f3 \ud83d\ude09",
+        "other": null
+    },
+    {
+        "name": "T\u00f4 Hu\u1ef3nh Ph\u00fac",
+        "hobby": "Phigros and games",
+        "review": "- Phuk m\u1eadp d\u1ec5 th\u01b0\u01a1ng <3\n- G\u00e1nh team \n- T\u1ea5t c\u1ea3 m\u1ecdi th\u1ee9 kh\u00e1c > dl",
+        "quote": "Treat people the way you want to be treated",
+        "other": null
+    },
+    {
+        "name": "Nguy\u1ec5n \u0110\u00ecnh Thi\u00ean Quang",
+        "hobby": "To\u00e1n, League of Legends",
+        "review": "- Ng\u01b0\u1eddi chinh ph\u1ee5c h\u1ebft t\u1ea5t c\u1ea3 c\u00e1c d\u00f2ng tr\u00ean b\u1ea3ng\n- Ch\u00faa t\u1ec3 t\u00f5ic",
+        "quote": "M\u00e0y s\u1ebd b\u1ecb ban v\u00ec t\u1ed9i tha ho\u00e1",
+        "other": "h\u00e0ng fake lo\u1ea1i 1 c\u1ee7a Ph\u1ea1m Ho\u00e0ng S\u01a1n"
+    },
+    {
+        "name": "\u0110\u1eb7ng Nguy\u1ec5n Tu\u1ea5n Th\u00e0nh",
+        "hobby": "Game, Th\u01a1 v\u00e0 S\u1eed",
+        "review": "- Qu\u00fd c\u00f4ng t\u1eed\n- \u00d4ng tr\u00f9m ki\u1ebfn th\u1ee9c\n- K\u1ebb s\u1edf h\u1eefu quy\u1ec1n debate lu\u00f4n th\u1eafng",
+        "quote": "Why have fun and be productive while you can be sad and extremely productive",
+        "other": null
+    },
+    {
+        "name": "L\u01b0 Tu\u1ea5n Th\u00e0nh",
+        "hobby": "To\u00e1n :v v\u00e0 \u0103n ",
+        "review": "- Wow r\u1ed3i chu m\u1ecf, m\u1eaft h\u00edp, \nc\u1edfi kh\u1ea9u trang ng\u1ea9ng m\u1eb7t l\u00ean t\u1ea1o g\u00f3c 25 \u0111\u1ed9 so v\u1edbi m\u1eb7t \u0111\u1ea5t, \n- Moi \u0111\u1ea7u \u0111\u1eb9p \n- Iem \u0111i\u1eb9p l\u00e9m ",
+        "quote": "Ch\u1ec9 c\u00f3 l\u00e0m ch\u1ecbu kh\u00f3 c\u1ea7n c\u00f9 b\u00f9 si\u00eang n\u0103ng. Ch\u1ec9 c\u00f3 l\u00e0m th\u00ec m\u1edbi c\u00f3 \u0103n.",
+        "other": "a^n +b^n =c^n v\u00f4 nghi\u1ec7m nguy\u00ean d\u01b0\u01a1ng v\u1edbi m\u1ecdi n>=3 "
+    },
+    {
+        "name": "Nguy\u1ec5n Ph\u1ea1m Thu\u1eadn Thi\u00ean",
+        "hobby": "JAVA",
+        "review": "- Chi\u1ebfn th\u1ea7n M\u1ef9 Tho\n- M\u00f9a thu l\u00e1 r\u01a1i nghi\u00eang\n- Crazy God",
+        "quote": "toi \u0111\u1eb9p trai",
+        "other": "T\u00f4i kh\u00f4ng ph\u1ea3i l\u00e0 m\u1ed9t b\u1ea3o t\u00e0ng ho\u00e1 th\u1ea1ch kh\u1ee7ng long th\u1eadt s\u1ef1 l\u1edbn, b\u1edfi v\u00ec I'm Not The Real Big Dinosaurs' Skeletons Museum"
+    },
+    {
+        "name": "Tr\u1ea7n Nguy\u1ec5n Y\u1ebfn Tr\u00e2m",
+        "hobby": null,
+        "review": "Ch\u1ecb y\u00eau tuy\u1ec7t v\u1eddi \u00f4ng m\u1eb7t tr\u0103ng",
+        "quote": null,
+        "other": null
+    },
+    {
+        "name": "L\u00ea Minh Tr\u1ecdng",
+        "hobby": "Sky",
+        "review": "-Chuy\u00ean nghi\u1ec7p ;)\n-Responsibility (l\u00e0 th\u1ee9 toi k c\u00f3)",
+        "quote": "Hmmmm...",
+        "other": null
+    },
+    {
+        "name": "Nguy\u1ec5n Huy T\u00f9ng",
+        "hobby": "Sticker con th\u1ecf \u0111i b\u1eb1ng l\u1ed7 tai",
+        "review": "- \u0110\u00f3m\n- IELTS 7.0\n- Mom",
+        "quote": "Top 4 nh\u1eefng ng\u01b0\u1eddi tui lu\u00f4n tin t\u01b0\u1edfng:\n1. Tr\u1ecbnh Tr\u1ea7n Ph\u01b0\u01a1ng Tu\u1ea5n r\u00e1ch\n2. Meo Meo r\u00e1ch\n3. Jack r\u00e1ch\n4. J97 r\u00e1ch",
+        "other": "Ti\u1ebfng tr\u1ed1ng tr\u01b0\u1edfng th\u00e0nh"
+    },
+    {
+        "name": "Tr\u01b0\u01a1ng Th\u1ee5c Uy\u00ean",
+        "hobby": "B\u1ea5t k\u1ef3 \u0111i\u1ec1u g\u00ec l\u00e0m tui vui ",
+        "review": "- \"T\u1ed1t b\u1ee5ng\"\n- \"Hi\u1ec1n l\u00e0nh\"\n- L\u1ef0C S\u0128 c\u1ee7a l\u1edbp\n- N\u01b0\u01a1ng n\u01b0\u01a1ng h\u1ee7y di\u1ec7t v\u0103n ch\u01b0\u01a1ng\n- K\u1ebb tr\u1ed1n ch\u1ea1y trong truth or dare\n- Ng\u01b0\u1eddi ti\u00eau di\u1ec7t th\u1ee9c \u0103n",
+        "quote": "Tui ch\u1ec9 mu\u1ed1n s\u1ed1ng m\u1ed9t cu\u1ed9c s\u1ed1ng h\u1ea1nh ph\u00fac :>",
+        "other": "V\u0169 (c\u1ea3 V\u0169. l\u1eabn b\u00e9 V\u0169)"
+    },
+    {
+        "name": "L\u00ea Quang \u1ea4n",
+        "hobby": "Ch\u1eedi Danh wack",
+        "review": "- Wack\n- Th\u00e0nh vi\u00ean th\u1ee9 37 c\u1ee7a l\u1edbp\n- C\u1ee9 g\u1ecdi t\u00f4i l\u00e0 huy",
+        "quote": "Tr\u00ean \u0111\u1eddi c\u00f3 2 lo\u1ea1i ng\u01b0\u1eddi: ng\u01b0\u1eddi kh\u00f4ng wack v\u00e0 Tr\u1ea7n Nguy\u1ec5n Thanh Danh",
+        "other": null
+    }
+];
+function showMem(x) {
+    var memed = mem[x-1];
+    document.getElementById("members").innerHTML +=
+        `<div id="mem-back" class="mem-back1">
+                <div class="mem-back" onclick=closeMem()></div>
+                <div class="info-box" >
+                    <h2>${memed.name}</h2>
+                    <div class="info-table">
+                        <div class="aa"><img src="mem-img/${x}.jpg" alt=""></div>
+                        <div class="table">
+                            <h3>Sở thích:</h3>
+                            <p>${memed.hobby}</p>
+                            <h3>Review:</h3>
+                            <p>${memed.review}</p>
+                            <h3>Quote:</h3>
+                            <p>${memed.quote}</p>${memed.other != null ? "<h3>Thêm:</h3><p>"+ memed.other +"</p>" : ""}
+                        </div>
+                    </div>
+                <div>
+            </div>`;
+}
+function closeMem() {
+    document.getElementById("members").removeChild(document.getElementById("mem-back"));
+}
